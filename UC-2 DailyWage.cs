3634e03cbc42dc5017c5ae5d1cae1b5047642d23
pagadalasunilkumar <131426667+pagadalasunilkumar@users.noreply.github.com>
2023-05-19 10:00:44 +0530

    Add files via upload

---
+++ UC-2 DailyWage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWagePROBLEMs
+{
+    internal class DailyWage
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+
+            int hourlyWageRate = 10; // Assuming an hourly wage rate of $10
+            int hoursWorked = 8; // Assuming the employee worked 8 hours
+
+            int dailyWage = hourlyWageRate * hoursWorked;
+            Console.WriteLine($"Employee's daily wage is: ${dailyWage}");
+
+            Console.ReadLine();
+        }
+    }
+}
